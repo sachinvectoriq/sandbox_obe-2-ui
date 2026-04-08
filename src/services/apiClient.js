e8c8dc7b9da91040c781f8eebb25c85d56f3b6c9
Solomonbhaskar <sbhaskar@vectoriq.ai>
2026-04-08 12:13:37 +0530

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL:
-    import.meta.env.VITE_API || 'https://app-ocm20-obe-dev-001-dgd8hbbyeyc2d2fv.eastus2-01.azurewebsites.net//',
+    import.meta.env.VITE_API || 'https://app-ka-sandbox-001.azurewebsites.net/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
