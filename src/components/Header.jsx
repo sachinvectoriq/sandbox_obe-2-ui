24bb8cedfde1cb99943c76f6e0672e79752bf733
Solomonbhaskar <sbhaskar@vectoriq.ai>
2026-04-23 00:35:06 +0530

    Update header title to 'OBE Knowledge Assistant Sandbox'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,7 +116,7 @@ const Header = () => {
       <div className="container p-4 w-[95%] max-w-[1400px] mx-auto flex justify-between items-center">
         <Logo />
         <h1 className="text-3xl font-semibold text-[#fcbc19]">
-          OBE Knowledge Assistant
+          OBE Knowledge Assistant Sandbox
         </h1>
 
         <div className="flex items-center gap-4">
